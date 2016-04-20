77f441d0f3f7682dd11ae6389e34544503a0496d
kwanjeeraw <kwanjeeraw@gmail.com>
2016-04-20 00:07:24 -0700

    bugs fixed

--- R/callBionet.R
+++ R/callBionet.R
@@ -7,12 +7,12 @@
 #'@param pval a numeric vector of p-values with name attributes
 #'@param fdr a numeric value specifying false discovery rate. Default is 0.05.
 #'@details
-#'The method \code{bionet} encapsulates the main functions from \pkg{\link{BioNet}} to compute a functional module or significanly changed subnetworks. 
+#'The method \code{bionet} encapsulates the main functions from \pkg{\link{BioNet}} to compute a functional module or significanly changed subnetworks.
 #'Those functions include: \code{\link{fitBumModel}}, \code{\link{scoreNodes}}, \code{\link{runFastHeinz}}.
 #'@return list of nodes and edges. The list contains the data frame of nodes and the data frame of edges.
 #'Return empty list of data frame if error or found nothing.
 #'@author Kwanjeera W \email{kwanich@@ucdavis.edu}
-#'@references Beisser D., Klau GW., Dandekar T., Müller T. and Dittrich MT. (2010) BioNet: an R-Package for the functional analysis of biological networks. Bioinformatics, 26(8):1129-30 
+#'@references Beisser D., Klau GW., Dandekar T., Müller T. and Dittrich MT. (2010) BioNet: an R-Package for the functional analysis of biological networks. Bioinformatics, 26(8):1129-30
 #'@references Dittrich MT., Klau GW., Rosenwald A., Dandekar T., Müller T. (2008) Identifying functional modules in protein-protein interaction networks: an integrated exact approach. Bioinformatics, 24(13):i223-31
 #'@seealso \code{\link{fitBumModel}}, \code{\link{scoreNodes}}, \code{\link{runFastHeinz}}
 #'@examples
@@ -32,8 +32,8 @@ callBionet <- function (edgelist, nodelist, pval, fdr){
     scoredNw = BioNet::scoreNodes(network = grph, fb = fbModel, fdr = fdr)
     mdule = NULL #set default
     mdule = BioNet::runFastHeinz(network = grph, scores = scoredNw)
-    
-    if(!is.null(mdule)){
+
+    if(length(igraph::E(mdule))>0){
       cat("Formatting subnetwork ...\n")
       ns = lapply(igraph::list.vertex.attributes(mdule),function(x) igraph::get.vertex.attribute(mdule,x))
       attb = data.frame((sapply(ns,c)), stringsAsFactors = FALSE)
@@ -41,7 +41,7 @@ callBionet <- function (edgelist, nodelist, pval, fdr){
       colnames(attb)[1] = "id" #set colname
       #attb$nodelabel = ""
       es = lapply(igraph::list.edge.attributes(mdule),function(x) igraph::get.edge.attribute(mdule,x))
-      pair = data.frame((sapply(es,c)))
+      pair = as.data.frame(es)
       pair = cbind(igraph::ends(mdule, igraph::E(mdule), names=TRUE), pair, stringsAsFactors=FALSE)
       colnames(pair) = c("source", "target", igraph::list.edge.attributes(mdule))
       cat("Returning subnetwork of ",nrow(attb)," nodes and ",nrow(pair)," edges...\n")
