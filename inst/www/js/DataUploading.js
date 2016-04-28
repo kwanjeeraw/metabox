08028092c0135ba54da5a3c064211a5aa3541b59
slfan2013 <fansili2013@gmail.com>
2016-04-28 14:35:45 -0700

    fdas

--- inst/www/js/DataUploading.js
+++ inst/www/js/DataUploading.js
@@ -1,27 +1,67 @@
-function choosefile(file){ //$("#display_choosefile").
 
-  if(file=="example"){
-    document.getElementById("display_choosefile").style.display ="none";
-  }else if(file=="aggregated"){
-    document.getElementById("display_choosefile").style.display ="block";
-    document.getElementById("display_choosefile").innerHTML =
-    "<p>Please read detailed explaination about the <a href='https://github.com/kwanjeeraw/mETABOX'>upload data format</a>. \
-    An example dataset is also <a href='https://github.com/kwanjeeraw/mETABOX'>provided</a>.</p>\
-    <label>Upload Aggregated Data</label> \
-    <input id='InputData' type='file'>";
-  }else if(file=="expression_feature_phenotype"){
-    document.getElementById("display_choosefile").style.display ="block";
-    document.getElementById("display_choosefile").innerHTML =
-    "<p>Please read detailed explaination about the <a href='https://github.com/kwanjeeraw/mETABOX'>upload data format</a>. \
-    If the order of features or subjects do not match, don\'t worry we can handle that but please read carefully with \
-    the <a href='https://github.com/kwanjeeraw/mETABOX'>upload data format</a>.</p>\
-    <p>Example datasets are also <a href='http://www.google.com'>provided</a> for expression, feature and phenotype seperately.\
-    </p>\
-    <div style='float:left'><label>Upload Expression Data</label> <input id='InputDataExp' type='file'></div>\
-    <div style='float:left'><label>Upload Feature Data</label> <input id='InputDataExp' type='file'></div>\
-    <div style='float:left'><label>Upload Phenotypen Data</label> <input id='InputDataExp' type='file'></div>";
-  }else{
-    return null;
-  }
+$(document).ready(function(){//wait untile the document is fully loaded.
 
-}
+    // Decide display input one file or three files.
+    $('input[name="uploadtype"]').click(function(){
+         if($(this).attr("value")=="upload_aggregated_datasets"){
+           $(".aggregated").show("fast");
+           $(".expression_feature_phenotype").hide("fast");
+
+         }else if($(this).attr("value")=="upload_expression_feature_phenotype_datasets"){
+           $(".aggregated").hide("fast");
+           $(".expression_feature_phenotype").show("fast");
+         }else{
+           $(".aggregated").hide("fast");
+           $(".expression_feature_phenotype").hide("fast");
+         }
+    });
+    $("#InputAggregatedData").on("change", function(){
+          if($("#InputAggregatedData")[0].files[0]){//make sure the file is loaded.
+              var showall = document.getElementsByClassName("showall");
+              for (var i = 0; i < showall.length; i ++) {
+                      showall[i].style.display = 'none';
+                  }
+                  document.getElementById("data_summary_tag_heading").innerHTML = "Experimental Design";
+
+
+
+          }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
