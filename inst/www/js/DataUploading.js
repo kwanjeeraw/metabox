c0c2f00e526f3b53da646d2418fb910447955c92
slfan2013 <fansili2013@gmail.com>
2016-04-30 22:16:18 -0700

    a

--- inst/www/js/DataUploading.js
+++ inst/www/js/DataUploading.js
@@ -18,12 +18,23 @@ $(document).ready(function(){//wait untile the document is fully loaded.
     var aggregated; //This is the session of list containing three data.frames ("expression","feature","phenotype")
     $("#InputAggregatedData").on("change", function(){
     //verify that a file is selected
-    if($("#csvfile")[0].files[0]){
+    if($("#InputAggregatedData")[0].files[0]){
 
       //clear state
       aggregated = null;
       var x = document.getElementById("InputAggregatedData");
-      document.getElementById("demo").innerHTML = x.files[0].name;
+      var req=ocpu.call("load_aggregated_data",{
+        file: $("#InputAggregatedData")[0].files[0],
+        type: x.files[0].name
+      },function(session){
+        aggregated = session;
+      }).fail(function(jqXHR){
+        errormsg(jqXHR.responseText)
+      });
+
+      document.getElementById("demo").innerHTML = "x.files[0].name";
+
+
     }
   });
 
