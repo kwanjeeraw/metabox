aa0d43a04dc6ea7fa5719d438ca9474a85e264ce
kwanjeeraw <kwanjeeraw@gmail.com>
2016-05-03 01:53:01 -0700

    add internal function

--- R/internalFormat.R
+++ R/internalFormat.R
@@ -3,50 +3,59 @@ formatPath.TRANSACTION <- function(graph){
   out <- tryCatch(
     {
       ## Set the name for the class
-      data.frame(t(sapply(graph$graph$relationships, 
+      data.frame(t(sapply(graph$graph$relationships,
                           function(x) list(source=x$startNode, target=x$endNode, type=x$type, datasource=paste0(x$properties["dataSource"],collapse = "||"), properties=paste0(x$properties["properties"],collapse = "||")))))
       },
     error=function(e) {
       message(e)
       cat("\n..RETURN empty list of relations")
       out = data.frame() # Choose a return value in case of error
-    })    
+    })
   return(out)
 }
 
 formatNode.TRANSACTION <- function(node){
   out <- tryCatch(
     {
-      data.frame(t(sapply(node, 
+      data.frame(t(sapply(node,
                           function(x) list(id=x$id, gid=x$properties$GID, nodename=x$properties$name, nodelabel=x$labels, nodexref=paste0(x$properties$xref,collapse = "||")))))
       },
     error=function(e) {
       message(e)
       cat("\n..RETURN empty list of relations")
       out = data.frame() # Choose a return value in case of error
-    })    
+    })
   return(out)
 }
 
 formatNode.TRANSACTION.ALL <- function(node){
   out <- tryCatch(
     {
-      data.frame(t(sapply(node, 
-                          function(x) list(id=x$id, gid=x$properties$GID, nodename=x$properties$name, nodelabel=x$labels, 
-                                           datasource=ifelse(!is.null(x$properties$dataSource),x$properties$dataSource,""), 
-                                           description=ifelse(!is.null(x$properties$description),x$properties$description,""), 
-                                           organism=ifelse(!is.null(x$properties$organism),x$properties$organism,""), 
+      data.frame(t(sapply(node,
+                          function(x) list(id=x$id, gid=x$properties$GID, nodename=x$properties$name, nodelabel=x$labels,
+                                           datasource=ifelse(!is.null(x$properties$dataSource),x$properties$dataSource,""),
+                                           description=ifelse(!is.null(x$properties$description),x$properties$description,""),
+                                           organism=ifelse(!is.null(x$properties$organism),x$properties$organism,""),
                                            synonyms=paste0(x$properties$synonyms,collapse = "||"), nodexref=paste0(x$properties$xref,collapse = "||")))))
     },
     error=function(e) {
       message(e)
       cat("\n..RETURN empty list of relations")
       out = data.frame() # Choose a return value in case of error
-    })    
+    })
   return(out)
 }
 
 formatId = function(x, y) {
   ind = which(y$gid == x)
   x = ifelse(length(ind)>0,y$id[ind],x)
+}
+
+formatNode.LIST = function(x,y,z){
+  nout = fetchNode(txtinput=x, nodetype=y, searchby=z)
+  if(nrow(nout)==0){
+    nout = data.frame(id=x, gid=x, nodename=x, nodelabel=Hmisc::capitalize(y), nodexref='', stringsAsFactors = FALSE)
+  }else{
+    nout = nout[,c(1:4,9)]
+  }
 }
