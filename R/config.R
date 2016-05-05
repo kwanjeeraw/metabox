ce70720099b514f01097fee432df95afec1dcca5
kwanjeeraw <kwanjeeraw@gmail.com>
2016-05-04 18:59:47 -0700

    remove import tag

--- R/config.R
+++ R/config.R
@@ -1,8 +1,9 @@
-#'@import RCurl jsonlite igraph WGCNA foreach doParallel
-#'@importFrom Hmisc capitalize
-#'@importFrom plyr rbind.fill
-#'@importFrom stringr str_trim
-#'@importFrom reshape2 melt
+#'# @import RCurl jsonlite igraph WGCNA foreach doParallel
+#'# @importFrom Hmisc capitalize
+#'# @importFrom plyr rbind.fill
+#'# @importFrom stringr str_trim
+#'# @importFrom reshape2 melt
+#'# @importFrom tm SnowballC wordcloud
 ##version
 #2.1.5
 #nld <- "http://grinn.genomecenter.ucdavis.edu:7474/db/data/cypher"
