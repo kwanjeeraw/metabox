cae2a9f72ee61ce397e1a2063beb7e4cbc161225
kwanjeeraw <kwanjeeraw@gmail.com>
2016-05-08 17:52:03 -0700

    edit doc

--- R/callMesh.R
+++ R/callMesh.R
@@ -2,6 +2,25 @@
 #'@description retrieve Mesh for a given pubchemID from PubChem
 #'@param pcid pubchem ID
 #'@return list of nodes and edges. The list contains the data frame of nodes or Mesh and the data frame of edges or annotation pair.
+#'list of nodes with the following components:
+#'
+#'\code{id} = mesh id
+#'
+#'\code{gid} = mesh id
+#'
+#'\code{nodename} = mesh name
+#'
+#'\code{nodelabel} = Mesh
+#'
+#'#'\code{nodexref} = mesh id
+#'edgelist with the following components:
+#'
+#'\code{source, target} = node
+#'
+#'\code{type} = ANNOTATION
+#'
+#'\code{datasource} = PUBCHEM
+#'
 #'Return empty list of data frame if error or found nothing.
 #'@author Kwanjeera W \email{kwanich@@ucdavis.edu}
 #'@examples
@@ -14,13 +33,13 @@ callMesh.default <- function(pcid){
       url = paste0('https://pubchem.ncbi.nlm.nih.gov/classification/cgi/classifications.fcgi?format=json&hid=1&search_uid_type=cid&search_uid=',pcid,'&search_type=tree')
       mesh = jsonlite::fromJSON(txt=url, simplifyVector = FALSE)$Hierarchies$Hierarchy[[1]]$Node
       attb = unique(plyr::ldply(lapply(mesh,formatMesh), data.frame))
-      pair = unique(cbind(attb$id,pcid))
-      colnames(pair) = c("source","target")
+      pair = unique(cbind(attb$id,pcid,"ANNOTATION","PUBCHEM"))
+      colnames(pair) = c("source","target","type","datasource")
       pair = as.data.frame(pair,stringsAsFactors = FALSE)
       list(nodes=attb, edges=pair)
     },error=function(e) {
       #message(e)
-      cat("\nUnknown compound: RETURN no result ..\n")
+      cat("Unknown compound: RETURN no result ..\n")
       list(nodes=data.frame(), edges=data.frame())
     })
   return(out)
