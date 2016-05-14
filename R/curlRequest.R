dc8e520f6e60982177282dfc72b1184dd53639a6
kwanjeeraw <kwanjeeraw@gmail.com>
2016-05-13 23:41:52 -0700

    update document

--- R/curlRequest.R
+++ R/curlRequest.R
@@ -1,18 +1,17 @@
 #'Execute query
-#'@description execute query using Cypher or url as an input. The function calls curlPerform from \pkg{RCurl} for sending HTTP request to the graph database.
-#'@usage curlRequest(param)
-#'@param cypher a string of Cypher (default) or url a string of url
+#'@description execute query using the function \code{\link{curlPerform}} from \pkg{RCurl} for sending HTTP request to the database.
 #'@return list or data frame. Return no data if found nothing.
 #'@author Kwanjeera W \email{kwanich@@ucdavis.edu}
 #'@references The RCurl package \url{http://www.inside-r.org/packages/cran/RCurl}
 #'@seealso \code{\link{curlPerform}}, \url{http://neo4j.com/docs/milestone/introduction.html}
 #'@examples
-#'# Query metabolites by database id
-#'#querystring <- "UNWIND ['MT-TF','MT-ND3'] AS x WITH x MATCH (node:Gene) WHERE node.name = x RETURN DISTINCT node"
-#'#result <- curlRequest(querystring)
+#'# Query by neo4j id
+#'#querystring <- "MATCH (n) WHERE ID(n) = 0 RETURN n"
+#'#result <- curlRequest.TRANSACTION(querystring)
+#'# Query by url
 #'#url <- "http://localhost:7474/db/data/node/306/relationships/out"
 #'#result <- curlRequest.URL(url)
-#cypher
+#legacy HTTP endpoint
 curlRequest <- function(cypher){
   h = RCurl::basicTextGatherer()
   tryCatch({
@@ -31,7 +30,8 @@ curlRequest <- function(cypher){
     result <- list() #return empty if not found
   })
 }
-#transaction
+#transaction HTTP endpoint
+#note: execute multiple statements available in neo4j 2.3.3
 curlRequest.TRANSACTION <- function(cypher){
   h = RCurl::basicTextGatherer()
   tryCatch({
@@ -45,82 +45,41 @@ curlRequest.TRANSACTION <- function(cypher){
                        writefunction = h$update,
                        verbose = FALSE
     )
-    #result <- RJSONIO::fromJSON(h$value())$results[[1]]$data
     result <- jsonlite::fromJSON(h$value(), simplifyDataFrame=FALSE)$results[[1]]$data
   }, error = function(err) {
     message(err)
     result <- list() #return empty if not found
   })
 }
-# #cypher
-# curlRequest.json <- function(cypher){
-#   h = RCurl::basicTextGatherer()
-#   tryCatch({
-#     url = paste0(nld,"cypher")
-#     RCurl::curlPerform(url=url,
-#                        userpwd = neu,
-#                        httpheader = c(Authorization = paste("Basic",RCurl::base64(neu))),
-#                        postfields=paste('query',RCurl::curlEscape(cypher), sep='='),
-#                        writefunction = h$update,
-#                        verbose = FALSE
-#     )
-#     result <- h$value() #return as json
-#   }, error = function(err) {
-#     message(err)
-#     result <- list() #return empty if not found
-#   })
-# }
-#
-# #url
-# curlRequest.URL <- function(url){
-#   h = RCurl::basicTextGatherer()
-#   tryCatch({
-#     RCurl::curlPerform(url=url,
-#                        userpwd = neu,
-#                        httpheader = c(Authorization = paste("Basic",RCurl::base64(neu))),
-#                        writefunction = h$update,
-#                        verbose = FALSE
-#     )
-#     result <- jsonlite::fromJSON(h$value(), simplifyVector = FALSE) #return as list
-#   }, error = function(err) {
-#     message(err)
-#     result <- list() #return empty if not found
-#   })
-# }
-# #url
-# curlRequest.URL.DFrame <- function(url){
-#   h = RCurl::basicTextGatherer()
-#   tryCatch({
-#     RCurl::curlPerform(url=url,
-#                        userpwd = neu,
-#                        httpheader = c(Authorization = paste("Basic",RCurl::base64(neu))),
-#                        writefunction = h$update,
-#                        verbose = FALSE
-#     )
-#     result <- jsonlite::fromJSON(h$value()) #return as data.frame
-#   }, error = function(err) {
-#     message(err)
-#     result <- data.frame() #return empty if not found
-#   })
-# }
-#
-# curlRequest.TRANSACTION <- function(cypher){
-#   h = RCurl::basicTextGatherer()
-#   tryCatch({
-#     #cypher = "MATCH ptw = (from:Protein)-[:CONTROL]->(to:Gene) WHERE from.GID = 'P14859' RETURN DISTINCT ptw LIMIT 2"
-#     #url = paste0(nld,"transaction/commit")
-#     url = "http://localhost:7474/db/data/transaction/commit"
-#     body = paste0("{\"statements\":[{\"statement\":\"",cypher,"\",\"resultDataContents\":[\"graph\"]}]}")
-#     RCurl::curlPerform(url=url,
-#                        userpwd = neu,
-#                        httpheader = c(Authorization = paste("Basic",RCurl::base64(neu)), 'Content-Type' = "application/json"),
-#                        postfields=body,
-#                        writefunction = h$update,
-#                        verbose = FALSE
-#     )
-#     result <- RJSONIO::fromJSON(h$value())$results[[1]]$data
-#   }, error = function(err) {
-#     message(err)
-#     result <- list() #return empty if not found
-#   })
-# }
+#url request to list
+curlRequest.URL <- function(url){
+  h = RCurl::basicTextGatherer()
+  tryCatch({
+    RCurl::curlPerform(url=url,
+                       userpwd = neu,
+                       httpheader = c(Authorization = paste("Basic",RCurl::base64(neu))),
+                       writefunction = h$update,
+                       verbose = FALSE
+    )
+    result <- jsonlite::fromJSON(h$value(), simplifyVector = FALSE) #return as list
+  }, error = function(err) {
+    message(err)
+    result <- list() #return empty if not found
+  })
+}
+#url request to data frame
+curlRequest.URL.DFrame <- function(url){
+  h = RCurl::basicTextGatherer()
+  tryCatch({
+    RCurl::curlPerform(url=url,
+                       userpwd = neu,
+                       httpheader = c(Authorization = paste("Basic",RCurl::base64(neu))),
+                       writefunction = h$update,
+                       verbose = FALSE
+    )
+    result <- jsonlite::fromJSON(h$value()) #return as data.frame
+  }, error = function(err) {
+    message(err)
+    result <- data.frame() #return empty if not found
+  })
+}
