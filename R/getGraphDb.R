a032e0d05e3964f6332d95e738ef53967f32112b
kwanjeeraw <kwanjeeraw@gmail.com>
2016-08-05 15:08:11 -0700

    new link

--- R/getGraphDb.R
+++ R/getGraphDb.R
@@ -1,6 +1,6 @@
 #'Get database location
 #'@description get database location of the current working envornment.
-#'By default the database lacation is http://localhost:7474/db/data/cypher
+#'By default the local Neo4j database location is http://localhost:7474/db/data/
 #'@usage getGraphDb()
 #'@return url of the database location.
 #'@author Kwanjeera W \email{kwanich@@ucdavis.edu}
@@ -11,7 +11,7 @@ getGraphDb <- function() UseMethod("getGraphDb")
 getGraphDb.default <- function(){
   if(foundDb()){
     getDbInfo()
-    cat(database.location)
+    cat(gsub("http://metaboxdb.fiehnlab.ucdavis.edu/db/data/","metabox is using a default human database.",database.location))
   }else{
     cat("No database found. Using BioNetwork and Pathway-based analyses will require database installation.")
   }
